c6b7204749812d48d10bdb271965950776973837
James Dordoy <me@jamesdordoy.co.uk>
2025-06-18 21:07:43 +0100

    Laravel 12 change

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     Lab404\Impersonate\ImpersonateServiceProvider::class,
 ];

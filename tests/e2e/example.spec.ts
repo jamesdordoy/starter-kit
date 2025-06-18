578bbede8cdf1580e2453a613355bc496a8de333
James Dordoy <me@jamesdordoy.co.uk>
2025-06-18 18:34:43 +0100

    Update example.spec.ts

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,14 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('login', async ({ page }) => {
-
-  test('test', async ({ page }) => {
-    await page.goto('https://starter-kit.test/');
-    await page.getByRole('link', { name: 'Log in' }).click();
-    await page.getByRole('textbox', { name: 'Email address' }).click();
-    await page.getByRole('textbox', { name: 'Email address' }).fill('admin@admin.com');
-    await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
-    await page.getByRole('textbox', { name: 'Password' }).fill('password');
-    await page.getByRole('button', { name: 'Log in' }).click();
-  });
+test('login test', async ({ page }) => {
+  await page.goto('https://starter-kit.test/');
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('admin@admin.com');
+  await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('password');
+  await page.getByRole('button', { name: 'Log in' }).click();
 });
